d6e9a24839f3fa7786db3575de5dc4a194d879e3
bao ngo <ngobao3107@gmail.com>
2021-06-24 22:04:31 +0700

    resolve confict videochat UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 // import React from 'react';
 import Home from './page/Home';
 // import './App.css';
+import React from 'react';
+import './App.css';
+import Video from './components/videochat/video/Video';
+import Chatbox from './components/videochat/chat/Chatbox';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      {/* <Home/> */}
+      <div className="main">
+        <Video />
+        <Chatbox />
+      </div>
     </div>
   );
 }
