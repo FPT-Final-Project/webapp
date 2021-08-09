e7702e04f92be10591a3d7fc3db46ab97be6ad44
NgoBao <ngobao3107@gmail.com>
2021-08-09 11:50:39 +0700

    resolve conflict

--- src/config/momo.config.ts
+++ src/config/momo.config.ts
@@ -1,5 +1,6 @@
 import { v1 as uuid } from 'uuid';
 import crypto from 'crypto';
+import appConfig from './app.config';
 
 const partnerCode = 'MOMO7OU020210728';
 const accessKey = 'D8h4XhEdfdGdMQ7p';
@@ -7,7 +8,7 @@ const secreteKey = 'oimuCa81icwpHAgnw44iHKiiK8sSX5dM';
 const momoEndpoint = 'https://test-payment.momo.vn/gw_payment/transactionProcessor';
 const orderInfo = 'pay with Momo';
 const requestType = 'captureMoMoWallet';
-const notifyUrl =`${appConfig.backendUrl}/payment/momo`;
+const notifyUrl = `${appConfig.backendUrl}/payment/momo`;
 const returnUrl = `${appConfig.appUrl}/app/appointment`;
 
 const momoRequest = async (
